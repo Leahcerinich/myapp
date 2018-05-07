fb45644ceb98687597e38f57e6d330c3c8bcbefd
Leah Cerinich <leah.cerinich@gmail.com>
2018-05-08 09:56:46 +1000

    update test files

--- test/blog.test.js
+++ test/blog.test.js
@@ -4,160 +4,128 @@ var jest = require('jest');
 var request = require('request'); // setting up the environment - requesting to the server
 var app = require('../server');
 var body = require('../app.js');
-var statusCode = require('');
-var base_url = 'http://localhost:3000/blog';
+var base_url = 'http://localhost:8080/blog/';
 
 
+// xdescribe("Server",() => {
+//     var server;
+//     beforeALL(() => {
+//         server = require("../app.js");
+//     });
+//     afterALL(() => {
+//         server.close();
+//     });
+// });
 
 
-// TODO:
-// Test that creates piece of Content, returns a 200.
+describe('Testing the API to GET retrieve all content', function () {
+    it('returns status code 200', function () {
+        request.get(base_url, function (err, res) {
+            var responseBody = [];
+            responseBody = JSON.parse(res.body);
 
-var expect = jest.expect;
-describe('API tests', function () {
-    // var content = {
-    //     name: 'integration test',
-    //     xxxx: 'xxxxx'
-    // },
-
-    describe('get all content', function () {
-        it('should retrieve all content', function (done) {
-            request(app).get('/content').end(function (err, res) {
-                expect(res.statusCode).toBe(200);
-                expect(res.body).toBe('array');
-                //expect(res.body).to.be.empty;
-                done();
-            });
-        });
-    });
-
-    // testing to check publish date is a valid date
-    describe('get all dates of content', function () {
-        it('should be date retrieve should be a valid date', function (done) {
-            request(app).get('/date').end(function (err, res) {
-                expect(statusCode).toBe(200);
-                expect(res.body.date).toBe('valid date');
-                done();
-            });
-        });
-    });
-
-    describe('POST a title', function() {
-        it('title should be equal or less than 140 characters', function(done) {
-            request(body).get('/title').end(function (err,res) {
-                expect(statusCode).toBe(200);
-                expect(res.body.title).toBe('title');
-                done();
-            });
-        });
-    });
-
-
-
-    describe('POST a piece of content', function () {
-        it('should create a single piece of content', function (done) {
-            request(body).post('/content').end(function (err, res) {
-                expect(res.statusCode).toBe(200);
-                expect(res.body.title).toBe('New title created');
-                content = res.body;
-                done();
-            });
-        });
-    });
-
-    describe('GET a peice of content', function () {
-        it('should retrieves a peice of content', function (done) {
-            request(body).get('/content').end(function (err, res) {
-                expect(res.statusCode).toBe(200);
-                expect(res.body.title).toBe('retrieve title of content');
-                content = res.body;
-                done();
-            });
-        });
-    });
-
-    describe('DELETE a peice of content', function () {
-        it('should remove a piece of content', function (done) {
-            request(body).delete('/content').end(function (err, res) {
-                expect(res.statusCode).toBe(200);
-                expect(res.body.title).toBe('title deleted');
-                content = res.body;
-                done();
-            });
+            // Check the number of records returned
+            // expect(responseBody.length).toEqual(1);
+            
+            for(var i=0; i < responseBody.length; i++) { // for loop
+                expect((responseBody[i])._id).toBeDefined();
+                console.log((responseBody[i])._id);
+            }
         });
     });
 });
-//---------------------------------------------------------------
+var test_id = '5ae7ff61adf651720afc810a';
 
-xdescribe('myapp', function () {
-    describe('GET /blog', function () {
-        xit('returns status code 200', function () {
+describe('testing the API can GET a signal piece of content', function() {
+    it('checks the all the fields of a record are returned and valid', function() {
 
-            request.get(base_url, function (err, res, body) {
-                expect(res.statusCode).toBe(200);
-            });
-        });
-        xit('retrieving all content', function () { // blocking out code another way to comment out..
-            request.get(base_url, function (err, res, body) {
-                console.log('Content contains:title,text,date');
-                console.log(body);
-                console.log('-----------------------');
-                parsed_body = JSON.parse(body);
-                console.log('Title is' + parsed_body[1].title);
-            });
-        });
-    });
-});
-//1.testing to to retrieve all blog content
-//2.if successful it will retrieve all blog posts
-//3.expect body to be all content and all blogs listed from the database
+        //Call the api url with an id
+        request.get(base_url + test_id, function(err,res){
 
+            //Retrieve the json response and parse
+            var responseBody = [];
+            responseBody = JSON.parse(res.body);
 
-xdescribe('myapp', function () {
-    xdescribe('POST /Blog', function () {
-        xit('returns status of 200', function () {
+            //Check the JSON object has a valid id
+            expect(responseBody._id).toBeDefined();
+            console.log(responseBody._id);
+            //Check the JSON object returns has a title
+            expect(responseBody.title).toBeDefined();
+            console.log(responseBody.title);
+            //Check the JSON object returns has a date
+            expect(responseBody.date).toBeDefined();
+            console.log(responseBody.date);
+            //Check the JSON object returns has text
+            expect(responseBody.text).toBeDefined();
+            console.log(responseBody.text);     
 
-            request.post(base_url, function (err, res, body) {
-                expect(res.statusCode).toBe(200);
-                console.log('new content created');
-            });
-        });
+            //Check that title is less than 140 characters
+            //expect the character length of the title to be less than 140
+            //var str = 'Character length of title';
+            //var res = str.substr(0,140);
 
-        xit('creating a new piece of content', function () {
-            request.post(base_url, function (err, res, body) {
-                expect(body).toBe('new piece of content');
-                console.log('newly created piece of content');
-            });
-        });
-    });
-});
+            // unit test?
+            describe('character length', function() {
+                it('test the character length of title to be <= 140', function() {
+                    expect(x).toBe(x <= 140);
 
-// 1. to see if the function of post is working
-// 2. testing to see if the you can create a new blog / post
-// 3.if successful it will show new blog created
-// 4.expect the body to be title text and date
-
-
-//. retrieveing content from a single id
-//1. testing the feature of retriving the body of one blog from a single id
-//3. want to test to see if the function of a new blog for single id
-
-/*    xdescribe('myapp', function() {
-        describe('GET /blog', function() {
-            it('returns status code of 200', function() {
-
-                request.post(base_url, function(err,res,body) {
-                    expect(res.statusCode).toBe(200);
-                    console.log('Success you have retrieved one single peice of content');
                 });
             });
 
-            xit('retrieving a new content from an ID', function() {
-                request.post(base_url,function(err,res,body) {
-                    expect(body).toBe('Title, date, text for one ID');
-                    console.log('Body contains title, text and date of a signal ID');
-                    console.log(body);
+            //Loop?
+            for(var i=0; i < 140; i++) { // for loop 
+                expect(responseBody.title).toBeDefined();
+                console.log(responsebody.title);
+
+                for(var i=0; responseBody.length; i++) {
+                    expect(responseBody.title).toBeDefined();
+                    
+            // else if statement
+
+            if (responseBody.title === 'title of content') {
+                if (title <= 140) {
+                    console.log ('new title created');
+                    else if (title > 140) {
+                        console.log('cannot create new title more than 140 characters');
+
+                    
+                    }
+                }
+            }
                 });
             });
-        });
-    }); */
+
+            //Check that title is less than date is a valid date format (dd/mm/yyyy)
+            //expect the date to be in a particullar format
+
+            var d = new Date('dd/mm/yyyy');
+            var n = d.getDate();
+            expect(responseBody.date).toBeDefined();
+            console.log('dd/mm/yyyy');
+        }); 
+    });
+});
+
+// testing to API to see if it can delete ind id 
+
+var test_id = '5ae7ff61adf651720afc810a'
+
+describe('testing the API to DELETE a peice of content', function() {
+    it('returns a status code of 200', function() {
+        request.delete(base_url + test_id, function(err,res) {
+        function empty() {
+            list.length = 0;
+        }
+        empty();
+        responseBody = JSON.parse(res.body);
+        console.log('Blog '+ blog.name +' was deleted.');
+        // because the response body is a Json object which is an array - I though to use this method of deleting array - which is a test_id
+
+ 
+
+
+
+
+
+
+
